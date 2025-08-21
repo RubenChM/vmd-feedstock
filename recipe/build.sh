418091d52a47b39a15b60b57df635d3ed510ea63
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-08-21 19:20:46 +0200

    Add osx variant

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,17 @@
 set -x
 
-# Use the bin names expected in Make-arch
-ln -sf $CC $BUILD_PREFIX/bin/gcc
-ln -sf $CXX $BUILD_PREFIX/bin/g++
+# Set the architecture and symlinks for compilers expected by Make-arch
+if [[ "${target_platform}" == "osx-64" ]];
+then
+    ARCH="MACOSXX86_64"
+    ln -sf $CC $BUILD_PREFIX/bin/cc
+    ln -sf $CXX $BUILD_PREFIX/bin/c++
+elif [[ "${target_platform}" == "linux-64" ]];
+then
+    ARCH="LINUXAMD64"
+    ln -sf $CC $BUILD_PREFIX/bin/gcc
+    ln -sf $CXX $BUILD_PREFIX/bin/g++
+fi
 
 # Compiling plugins from source code
 # http://www.ks.uiuc.edu/Research/vmd/plugins/doxygen/compiling.html
@@ -10,7 +19,7 @@ export TCLINC=-I$BUILD_PREFIX/include
 export TCLLIB=-L$BUILD_PREFIX/lib
 
 cd plugins
-make LINUXAMD64 TCLINC="$TCLINC" TCLLIB="$TCLLIB"
+make $ARCH TCLINC="$TCLINC" TCLLIB="$TCLLIB"
 
 export PLUGINDIR=$SRC_DIR/vmd-1.9.3/plugins
 make distrib
@@ -22,7 +31,7 @@ mv LICENSE ..
 export CPATH="$BUILD_PREFIX/include:$PREFIX/include:$CPATH"
 export VMDINSTALLBINDIR=$PREFIX/bin
 export VMDINSTALLLIBRARYDIR=$PREFIX/lib/vmd
-./configure LINUXAMD64 TK TCL #NETCDF PTHREADS OPENGL OPENGLPBUFFER XINPUT COLVARS
+./configure $ARCH TK TCL #NETCDF PTHREADS OPENGL OPENGLPBUFFER XINPUT COLVARS
 ./configure
 cd src
 make
