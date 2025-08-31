c84ccaef457ba8cca528db73534f5e1494cb24a2
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-08-31 20:25:02 +0200

    Update Makefile reference

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,12 +32,13 @@ export CPATH="$BUILD_PREFIX/include:$PREFIX/include:$CPATH"
 export VMDINSTALLBINDIR=$PREFIX/bin
 export VMDINSTALLLIBRARYDIR=$PREFIX/lib/vmd
 if [[ "$target_platform" == "osx-64" ]]; then
-    ln -sf $BUILD_PREFIX/lib/libtcl8.5.dylib $BUILD_PREFIX/lib/libtcl8.5-x11.dylib
+    #ln -sf $BUILD_PREFIX/lib/libtcl8.5.dylib $BUILD_PREFIX/lib/libtcl8.5-x11.dylib
     ./configure $ARCH TCL LP64
 else
     ./configure $ARCH TK TCL #NETCDF PTHREADS OPENGL OPENGLPBUFFER XINPUT COLVARS
 fi
 ./configure
 cd src
+sed 's/libtcl8.5-x11/libtcl8.5/g' Makefile
 make
 make install
