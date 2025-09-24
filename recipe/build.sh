8bd2a49c5978af36b312315465a4cf0fc737739b
Rubén Chaves <ruben.chaves@irbbarcelona.org>
2025-09-24 14:32:01 +0200

    remove  sed command

--- recipe/build.sh
+++ recipe/build.sh
@@ -32,13 +32,11 @@ export CPATH="$BUILD_PREFIX/include:$PREFIX/include:$CPATH"
 export VMDINSTALLBINDIR=$PREFIX/bin
 export VMDINSTALLLIBRARYDIR=$PREFIX/lib/vmd
 if [[ "$target_platform" == "osx-64" ]]; then
-    #ln -sf $BUILD_PREFIX/lib/libtcl8.5.dylib $BUILD_PREFIX/lib/libtcl8.5-x11.dylib
-    ./configure $ARCH TCL LP64
+    ./configure $ARCH FLTKOPENGL TCL LP64 # FLTK COLVARS IMD TK SILENT PTHREADS LIBTACHYON ACTC OPENCL CUDA
 else
     ./configure $ARCH TK TCL #NETCDF PTHREADS OPENGL OPENGLPBUFFER XINPUT COLVARS
 fi
 ./configure
 cd src
-sed -i 's/libtcl8.5-x11/libtcl8.5/g' Makefile
 make
 make install
